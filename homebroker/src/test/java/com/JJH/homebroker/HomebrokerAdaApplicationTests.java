e38b9170968a9b976aec696841ec7733ed59fdb2
HepoHB <NeroHaziel@outlook.com>
2024-09-21 00:36:40 -0300

    Criação do Projeto com Spring e implementação das dependências JPA e H2 no Maven

---
+++ homebroker/src/test/java/com/JJH/homebroker/HomebrokerAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JJH.homebroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebrokerAdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
