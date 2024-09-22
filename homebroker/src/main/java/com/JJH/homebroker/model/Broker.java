8b945f9bde405f79f98d3029740728dbdd6493ba
HepoHB <NeroHaziel@outlook.com>
2024-09-21 23:12:00 -0300

    Broker Mapping -- Needs repair

--- homebroker/src/main/java/com/JJH/homebroker/model/Broker.java
+++ homebroker/src/main/java/com/JJH/homebroker/model/Broker.java
@@ -1,5 +1,25 @@
 package com.JJH.homebroker.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Getter @Setter
+@Entity
+@RequestMapping("api/v1/brokers")
 public class Broker{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "broker_id", nullable = false)
+    private Long id;
+
+    @Column(name = "broker_name", nullable = false)
+    private String name;
+
+    /*@Column(name = "broker_fees")         Talvez seja necessária outra classe.
+    private Map<String, brokerFee> fee;*/
 
 }
