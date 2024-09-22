a62104fab4958a1f4d9e24bace571413ee163cc0
HepoHB <NeroHaziel@outlook.com>
2024-09-21 23:23:03 -0300

    Transaction Mapping

--- homebroker/src/main/java/com/JJH/homebroker/model/Transaction.java
+++ homebroker/src/main/java/com/JJH/homebroker/model/Transaction.java
@@ -1,4 +1,29 @@
 package com.JJH.homebroker.model;
 
-public class Transaction {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@Entity
+@RequestMapping("api/v1/transactions")
+public class Transaction{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "tramsaction_id", nullable = false)
+    private long id;
+
+    @Column(name = "transaction_amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "transaction_date_time", nullable = false)
+    private LocalDateTime transactionDateTime;
+
+    @Column(name = "transaction_user", nullable = false)
+    private User user;
+
 }
