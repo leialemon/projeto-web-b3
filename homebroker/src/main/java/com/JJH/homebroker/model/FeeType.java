225136666c89b79e5479aade74194b1455e02119
HepoHB <NeroHaziel@outlook.com>
2024-09-23 17:03:32 -0300

    FeeType Update

--- homebroker/src/main/java/com/JJH/homebroker/model/FeeType.java
+++ homebroker/src/main/java/com/JJH/homebroker/model/FeeType.java
@@ -1,8 +1,11 @@
 package com.JJH.homebroker.model;
 
 public enum FeeType {
-    TAX,
-    BROKERAGE;
+    CUSTODY_TAX,
+    BROKERAGE,
+    EMOLUMENT,
+    LIQUIDATION_FEE,
+    INCOME_TAX;
 
     //TODO: criar regras de cálculo e passar no construtor do enum.
 }
