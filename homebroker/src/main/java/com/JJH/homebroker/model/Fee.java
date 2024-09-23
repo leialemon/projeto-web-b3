8277b35380a423fb6bb8d8e9fb122647425d1c1f
leialemon <julianabarrosf@protonmail.com>
2024-09-23 11:11:36 -0300

    Criação das classes Fee e FeeType

---
+++ homebroker/src/main/java/com/JJH/homebroker/model/Fee.java
@@ -0,0 +1,7 @@
+package com.JJH.homebroker.model;
+
+public class Fee {
+    private FeeType type;
+    private String name;
+    private Double amount;
+}
---
+++ homebroker/src/main/java/com/JJH/homebroker/model/FeeType.java
@@ -0,0 +1,8 @@
+package com.JJH.homebroker.model;
+
+public enum FeeType {
+    TAX,
+    BROKERAGE;
+
+    //TODO: criar regras de cálculo e passar no construtor do enum.
+}
