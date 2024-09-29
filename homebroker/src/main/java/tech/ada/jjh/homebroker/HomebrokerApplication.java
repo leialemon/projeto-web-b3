215e91fa426a9932873b180cb0539b3172b1ad88
HepoHB <NeroHaziel@outlook.com>
2024-09-29 19:07:39 -0300

    .

--- homebroker/src/main/java/tech/ada/jjh/homebroker/HomebrokerApplication.java
+++ homebroker/src/main/java/tech/ada/jjh/homebroker/HomebrokerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomebrokerApplication{
-
 	public static void main(String[] args){
 		SpringApplication.run(HomebrokerApplication.class, args);
 	}
-}
 
+}
