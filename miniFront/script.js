7b697e6547e8bc36520908348b56494e1965925b
Juliana Barros <julianabarrosf@protonmail.com>
2024-09-29 22:04:05 -0300

    Atualizações no minifront

--- miniFront/script.js
+++ miniFront/script.js
@@ -5,17 +5,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
   switch(pagina){
     case "/home/juliana/repositorios/Ada-Homebroker/miniFront/buscarUsuario.html":
-      buscarUser();
       console.log('Executando buscarUser()');
+      buscarUser();
       break;
     case "/home/juliana/repositorios/Ada-Homebroker/miniFront/cadastrarUsuario.html":
-      cadastrarUsuario();
       console.log('Executando cadastrarUsuario()');
+      cadastrarUsuario();
       break;
     case "/home/juliana/repositorios/Ada-Homebroker/miniFront/verUsuarios.html":
-      mostrarUsuarios();
       console.log('Executando mostrarUsuarios()');
-      break;  
+      mostrarUsuarios();
+      break;
+    case "/home/juliana/repositorios/Ada-Homebroker/miniFront/cadastrarAcao.html":
+      console.log('Cadastrando Ação');  
+      cadastrarAcao();
+      break;
+    case "/home/juliana/repositorios/Ada-Homebroker/miniFront/verAcoes.html":
+      console.log('Buscando ações');
+      verAcoes();
+      break;
+    case "/home/juliana/repositorios/Ada-Homebroker/miniFront/fazerTransacao.html":
+      console.log('Fazendo Transação');
+      fazerTransacao();
+      break;
+    case  "/home/juliana/repositorios/Ada-Homebroker/miniFront/criarOrdem.html":
+      console.log('Criando ordem');
+      criarOrdem();
+      break;          
   }
 
 
@@ -46,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
       })
       .then(data => {
         console.log('Sucesso:', data);
-        document.getElementById('responseDiv').innerText = 'Usuário cadastrado com sucesso! Resposta: ' + JSON.stringify(data);
+        document.getElementById('responseDiv').innerText = 'Usuário cadastrado com sucesso! \nResposta do servidor: ' + JSON.stringify(data, null, 2);
       })
       .catch((error) => {
         console.error('Erro:', error);
@@ -68,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
       return response.json();  
     })
     .then(data => {
-      document.getElementById('responseDiv').innerText = 'Resposta do servidor: ' + JSON.stringify(data);
+      document.getElementById('responseDiv').innerText = 'Resposta do servidor: ' + JSON.stringify(data, null, 2);
     })
     .catch(error => {
       document.getElementById('responseDiv').innerText = 'Erro: ' + error.message;
@@ -80,7 +96,8 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('entrou no método');
     const btn = document.getElementById('buscarUser');
     if (btn) {
-      btn.addEventListener('click', function() {
+      btn.addEventListener('click', function(event) {
+        event.preventDefault();
         const cpf = document.getElementById('bcpf').value;
         console.log('CPF a ser buscado:', cpf);
         if (!cpf) {
@@ -102,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(data => {
           console.log('Dados recebidos:', data);
-          document.getElementById('responseDiv').innerText = 'Resposta do servidor: ' + JSON.stringify(data);
+          document.getElementById('responseDiv').innerText = 'Resposta do servidor: ' + JSON.stringify(data, null, 2);
         })
         .catch(error => {
           console.error('Erro ao buscar usuário:', error);
@@ -114,10 +131,102 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
   
-
+  function cadastrarAcao(){
+    document.getElementById('cadastrarAcao').addEventListener('submit', function (event) {
+      event.preventDefault();
   
+      const formData = {
+        price: document.getElementById('fprice').value,
+        name: document.getElementById('fname').value,
+        ticker: document.getElementById('fticker').value
+      };
   
-
-
+      fetch('http://localhost:8080/api/v1/stocks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Erro HTTP: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Sucesso:', data);
+        document.getElementById('responseDiv').innerText = 'Ação cadastrada com sucesso! \nResposta do servidor ' + JSON.stringify(data, null, 2);
+      })
+      .catch((error) => {
+        console.error('Erro:', error);
+        document.getElementById('responseDiv').innerText = 'Erro: ' + error.message;
+      });
+    });
+  }
   
+  function verAcoes(){
+    document.getElementById('enviarBtn').addEventListener('click', function (){
+      fetch('http://localhost:8080/api/v1/stocks', {
+        method: 'GET'
+      })
+      .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro na requisição: ' + response.status);
+        }
+        return response.json();  
+      })
+      .then(data => {
+        document.getElementById('responseDiv').innerText = 'Resposta do servidor: ' + JSON.stringify(data, null, 2);
+      })
+      .catch(error => {
+        document.getElementById('responseDiv').innerText = 'Erro: ' + error.message;
+      });
+    });
+  }
+
+  function fazerTransacao(){
+    document.getElementById('fazerTransacao').addEventListener('submit', function (event) {
+      event.preventDefault();
+      let tipo;
+        if (document.getElementById('deposito').checked){
+          tipo = document.getElementById('deposito').value;
+        } else if (document.getElementById('saque').checked) {
+          tipo = document.getElementById('saque').value;
+        } 
+      
+      const formData = {
+        userCpf : document.getElementById('fcpf').value,
+        amount : document.getElementById('fprice').value,
+        type: tipo
+      };
+      fetch('http://localhost:8080/api/v1/transactions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Erro HTTP: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Sucesso:', data);
+        document.getElementById('responseDiv').innerText = 'Transação concluída! \nResposta do servidor ' + JSON.stringify(data, null, 2);
+      })
+      .catch((error) => {
+        console.error('Erro:', error);
+        document.getElementById('responseDiv').innerText = 'Erro: ' + error.message;
+      });  
+    
+    })
+    
+            
+  }
+
+  function criarOrdem(){}
+
 });
