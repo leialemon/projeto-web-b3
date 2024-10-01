5976667c1a8f2b0572892669ae5d393e9cb63869
Juliana Barros <julianabarrosf@protonmail.com>
2024-10-01 15:12:10 -0300

    correção nos scripts

--- scripts/powershell-homebroker-firefox.ps1
+++ scripts/powershell-homebroker-firefox.ps1
@@ -1,8 +1,8 @@
 Set-Location -Path "..\miniFront"
 
-Start-Process "chrome.exe" -ArgumentList "index.html"
+Start-Process "firefox.exe" "index.html"
 
-Set-Location -Path "..\..\homebroker\target"
+Set-Location -Path "..\homebroker\target"
 
 Start-Process "java.exe" "-jar homebroker-0.0.1-SNAPSHOT.jar"
 
