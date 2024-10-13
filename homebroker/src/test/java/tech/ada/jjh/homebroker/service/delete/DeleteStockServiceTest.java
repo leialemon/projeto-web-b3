724297966b38b3f13cb44c4241121a160cf2bd52
Juliana Barros <julianabarrosf@protonmail.com>
2024-10-13 09:32:47 -0300

    Criação de testes unitários para DeleteStockService

---
+++ homebroker/src/test/java/tech/ada/jjh/homebroker/service/delete/DeleteStockServiceTest.java
@@ -0,0 +1,44 @@
+package tech.ada.jjh.homebroker.service.delete;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.ada.jjh.homebroker.config.EntityNotFoundException;
+import tech.ada.jjh.homebroker.model.Stock;
+import tech.ada.jjh.homebroker.repository.StockRepository;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteStockServiceTest {
+
+    @InjectMocks
+    DeleteStockService deleteStockService;
+
+    @Mock
+    StockRepository stockRepository;
+
+    @Test
+    void mustSuccessfullyDeleteAnExistingStock() {
+        Stock stock = new Stock();
+        stock.setTicker("TESTE");
+        Mockito.when(stockRepository.findByTicker("TESTE")).thenReturn(Optional.of(stock));
+
+        deleteStockService.deleteStockByTicker("TESTE");
+
+        Mockito.verify(stockRepository, Mockito.times(1)).delete(stock);
+    }
+
+    @Test
+    void mustReturnEntityNotFoundExceptionSinceTheStockDoesNotExist(){
+        Mockito.when(stockRepository.findByTicker("TESTE")).thenReturn(Optional.empty());
+
+        RuntimeException test = Assertions.assertThrows(EntityNotFoundException.class, () -> deleteStockService.deleteStockByTicker("TESTE"));
+
+        Assertions.assertNotNull(test);
+    }
+}
