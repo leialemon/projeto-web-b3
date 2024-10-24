b43871859f4b45c0bae1bf657cd8122126b310a8
HepoHB <NeroHaziel@outlook.com>
2024-10-24 17:58:09 -0300

    TransactionControllerTest

--- homebroker/src/test/java/tech/ada/jjh/homebroker/controller/TransactionControllerTest.java
+++ homebroker/src/test/java/tech/ada/jjh/homebroker/controller/TransactionControllerTest.java
@@ -1,4 +1,51 @@
 package tech.ada.jjh.homebroker.controller;
 
-public class TransactionControllerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tech.ada.jjh.homebroker.dto.TransactionDTORequest;
+import tech.ada.jjh.homebroker.dto.TransactionDTOResponse;
+import tech.ada.jjh.homebroker.service.create.CreateTransactionService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class TransactionControllerTest {
+    private MockMvc mockMVC;
+
+    @Mock
+    private CreateTransactionService createTransactionService;
+    @InjectMocks
+    private TransactionController transactionController;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        mockMVC = MockMvcBuilders.standaloneSetup(transactionController).build();
+
+    }
+
+    @Test
+    void testCreateTransaction() throws Exception{
+        TransactionDTOResponse transactionResponse = new TransactionDTOResponse();
+        when(createTransactionService.createTransaction(any(TransactionDTORequest.class))).thenReturn(transactionResponse);
+        mockMVC.perform(post("/api/v1/transactions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"stockId\":1, \"quantity\":10, \"price\":\"150.0\"}"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        verify(createTransactionService).createTransaction(any(TransactionDTORequest.class));
+
+    }
+
 }
